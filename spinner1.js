c1411cffd75cc2de1d5044084e9126e12e4c851b
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-04-16 15:08:11 -0400

    finishing spinning function

--- spinner1.js
+++ spinner1.js
@@ -14,3 +14,8 @@ setTimeout(() => {
   // Need to escape the backslash since it's a special character.
   process.stdout.write('\r\\   '); 
 }, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r-|  \n');
+}, 900);
+
